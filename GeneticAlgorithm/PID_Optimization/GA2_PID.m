07ab4d2c1bbc81349def658bdf1ca5ff3fb0a042
gitupmak <aslamece1097@gmail.com>
2021-08-15 22:17:03 +0530

    .

--- GeneticAlgorithm/PID_Optimization/GA2_PID.m
+++ GeneticAlgorithm/PID_Optimization/GA2_PID.m
@@ -1,6 +1,7 @@
 %% ITAE =Intergral Time Absolute Error is used as  Performance measure for optimization
-%Cost function GA2_PID is applied to Optimization tool.
-% Initialize PID gain, k
+% Cost function GA2_PID is applied to Optimization tool.
+% Initialize PID gain, k(kp ki kd)
+% OptimizePID.slx is the PID controller for DC motor design in Simulink 
 function f = GA2_PID(k)
     assignin('base', 'k' , k);
     sim('OptimizePID.slx');
